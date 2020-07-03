293a9059ce4df9cdfe404cce82a2cc751bbce3e0
david <fremenida@gmail.com>
2020-07-03 20:51:20 +0200

    modificacion html+css+js

--- js/index.js
+++ js/index.js
@@ -5,7 +5,17 @@ $(document).ready(()=>{
         $("#datospersonales").show()
         $("#formacionacademica").hide();
         $("#experienciaprofesional").hide();
+    $("#btnFormacionAcademica").click(()=>{
+        $("#formacionacademica").show()
+        $("#datospersonales").hide();
+        $("#experienciaprofesional").hide();
+    $("#btnExperienciaProfesional").click(()=>{
+        $("#experienciaprofesional").show();
+        $("#formacionacademica").hide()
+        $("#datospersonales").hide();
 
+    })
+    })
     })
 
 })
